84b1c30164102126e0dcf9f5e9af3ae6524cf209
Charles Ofria <charles.ofria@gmail.com>
2022-01-13 09:48:29 -0500

    Refactored command-line interface.

--- Wordle-CommandLine.cpp
+++ Wordle-CommandLine.cpp
@@ -35,6 +35,13 @@ private:
     }
   }
 
+  template <typename... Ts>
+  void Error(Ts... args) {
+    std::cerr << emp::ANSI_Red() << "Error: " << emp::ANSI_Reset()
+              << emp::to_string(std::forward<Ts>(args)...)
+              << std::endl;
+  }
+
 public:
   WordleDriver()
   : word_set5(wordlist5)
@@ -44,19 +51,29 @@ public:
   WordleDriver(const WordleDriver &) = delete;
   WordleDriver(WordleDriver &&) = delete;
 
-  void Start() {
+  void PrintHelp() {
     std::cout << "Wordle Analyzer!\n"
-      << "Type clue followed by a word guess and the result to get more information.\n"
+      << "Type 'clue' followed by a word guess and the result to get more information.\n"
       << "Results should be in the form: N=Nowhere, E=Elsewhere, H=Here\n"
       << "  Example: \"clue start EHNNN\" would indicate that there is an 's', but not at\n"
       << "           the front, a 't' second, no 'a's or 'r's, and no additional 't's.\n"
-      << "Other commands:\n"
-      << "  exit : quit the program.\n"
-      << "  status : show the current clue stack.\n"
-      << "  clear : erase all current clues."
+      << "Commands:\n"
+      << "   clue   : provide a new clue and its result.\n"
+      << "            format: clue [word] [result]\n"
+      << "   -help  : Provide addition information about a command.\n"
+      << "            format: help [command]\n"
+      << "   quit   : exit the program.\n"
+      << "   reset  : erase all current clues.\n"
+      << "   status : show the current clue stack.\n"
+      << "   -words : list top legal words (type 'help words' for full information).\n"
+      << "            format: words [sort=alpha] [count=10] [output=screen]\n"
+      << "All commands can be shortened to just their first letter.\n"
+      << std::endl;
     ;
-  
-    std::cout << "Preprocessing...\n"
+  }
+
+  void Process() {
+    std::cout << "Processing...\n"
               << "-----------------------------------------------------" << std::endl;
 
     while (!word_set5.Preprocess()) {
@@ -64,69 +81,91 @@ public:
       if (progress % 2 == 0) { std::cout << '#'; std::cout.flush(); }
     }
     std::cout << "\n..." << word_set5.GetSize() << " words are analyzed; " << word_set5.GetNumResults() << " results each..." << std::endl;
+  }
 
-    // Collect inputs.
-    std::string input;
-    emp::vector<std::string> args;
-    while (true) {
-      std::cout << "> ";
-      std::cout.flush();
-      getline(std::cin, input);
-      emp::slice(input, args, ' ');
-
-      if (args[0] == "clue") {
-        if (args.size() != 3) {
-          std::cout << "Error: 'clue' command requires exactly two arguments." << std::endl;
-        }
-        else if (args[1].size() != word_size) {
-          std::cout << "Error: Word size " << word_size << " currently active." << std::endl;
-        }
-        else if (args[2].size() != word_size) {
-          std::cout << "Error: Results size must also be " << word_size << " characters." << std::endl;
-        }
-        else {
-          emp::vector<Status> clue_status(word_size);
-          for (size_t i = 0; i < word_size; ++i) {
-            switch (args[2][i]) {
-            case 'h': case 'H': clue_status[i] = Status::HERE; break;
-            case 'e': case 'E': clue_status[i] = Status::ELSEWHERE; break;
-            case 'n': case 'N': clue_status[i] = Status::NOWHERE; break;
-            default:
-              std::cout << "Error: Unknown status '" << args[2][i] << "'; using 'none'." << std::endl;
-              clue_status[i] = Status::NONE;
-            }
-          }
-          words.push_back(args[1]);
-          status.push_back(clue_status);
-        }
-      }
-
-      else if (args[0] == "exit" || args[0] == "quit") break;
-
-      else if (args[0] == "status") {
-        std::cout << words.size() << " clues currently enforced.\n";
-        for (size_t word_id = 0; word_id < words.size(); ++word_id) {
-          std::cout << "  ";
-          for (size_t let_id = 0; let_id < word_size; ++let_id) {
-            switch (status[word_id][let_id]) {
-              case Status::HERE:      std::cout << emp::ANSI_Green()  << emp::ANSI_BlackBG(); break;
-              case Status::ELSEWHERE: std::cout << emp::ANSI_Yellow() << emp::ANSI_BlackBG(); break;
-              case Status::NOWHERE:   std::cout << emp::ANSI_Black()  << emp::ANSI_WhiteBG(); break;
-              case Status::NONE:      std::cout << emp::ANSI_Red()    << emp::ANSI_BlackBG(); break;
-            }
-            std::cout << words[word_id][let_id];
-          }
-          std::cout << emp::ANSI_Reset() << "\n";
-        }
-        std::cout.flush();
-      }
-
-      else {
-        std::cout << "Error: Unknown command '" << args[0] << "'." << std::endl;
-      }
+  void CommandClue(const std::string & clue_word, const std::string clue_result) {
+    if (clue_word.size() != word_size) {
+      Error("Word size ", word_size, " currently active.");
+      return;
     }
 
+    if (clue_result.size() != word_size) {
+      Error("Results size must be the same as guess (", word_size, ").");
+      return;
+    }
+
+    emp::vector<Status> clue_status(word_size);
+    for (size_t i = 0; i < word_size; ++i) {
+      switch (clue_result[i]) {
+      case 'h': case 'H': clue_status[i] = Status::HERE; break;
+      case 'e': case 'E': clue_status[i] = Status::ELSEWHERE; break;
+      case 'n': case 'N': clue_status[i] = Status::NOWHERE; break;
+      default:
+        Error("Unknown status '", clue_result[i], "'; using 'none'.");
+        clue_status[i] = Status::NONE;
+      }
+    }
+    words.push_back(clue_word);
+    status.push_back(clue_status);
   }
+
+  bool ProcessCommandLine() {
+    std::string input;
+    emp::vector<std::string> args;
+
+    std::cout << "> ";
+    std::cout.flush();
+    getline(std::cin, input);
+    emp::slice(input, args, ' ');
+
+    if (args[0] == "clue" || args[0] == "c") {
+      if (args.size() != 3) {
+        std::cout << "Error: 'clue' command requires exactly two arguments." << std::endl;
+      }
+      else CommandClue(args[1], args[2]);
+      return true;
+    }
+
+    if (args[0] == "exit" || args[0] == "quit" || args[0] == "q") return false;
+
+    if (args[0] == "reset" || args[0] == "r") {
+      std::cout << "Clearing all current clues." << std::endl;
+      words.resize(0);
+      status.resize(0);
+      return true;
+    }
+
+    if (args[0] == "status" || args[0] == "s") {
+      std::cout << words.size() << " clues currently enforced.\n";
+      for (size_t word_id = 0; word_id < words.size(); ++word_id) {
+        std::cout << "  ";
+        for (size_t let_id = 0; let_id < word_size; ++let_id) {
+          switch (status[word_id][let_id]) {
+            case Status::HERE:      std::cout << emp::ANSI_Green()  << emp::ANSI_BlackBG(); break;
+            case Status::ELSEWHERE: std::cout << emp::ANSI_Yellow() << emp::ANSI_BlackBG(); break;
+            case Status::NOWHERE:   std::cout << emp::ANSI_Black()  << emp::ANSI_WhiteBG(); break;
+            case Status::NONE:      std::cout << emp::ANSI_Red()    << emp::ANSI_BlackBG(); break;
+          }
+          std::cout << words[word_id][let_id];
+        }
+        std::cout << emp::ANSI_Reset() << "\n";
+      }
+      std::cout.flush();
+      return true;
+    }
+
+    std::cout << "Error: Unknown command '" << args[0] << "'." << std::endl;
+    return true;
+  }
+
+  void Start() {
+    PrintHelp();
+    Process();
+
+    // Collect inputs.
+    while (ProcessCommandLine());  // Process the command line until it returns 'false'.
+  }
+
 };
 
 int main()
